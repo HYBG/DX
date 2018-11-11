3baf77dfa5189ef26402cc13cd8c0ec0a5fb0da5
alexshawliu <33888908+alexshawliu@users.noreply.github.com>
2018-11-11 20:17:52 +0800

    update

--- masque/masque.js
+++ masque/masque.js
@@ -9,19 +9,62 @@ GAME_PLAYER_STATUS_INGAME = 2;
 
 GAME_PLAYER_INIT_MONEY = 6;
 
-function masque(_uid,_nick,_img,_pcnt){
+GAME_MSG_FULL = 'GAME_MSG_FULL';
+GAME_MSG_DONE = 'GAME_MSG_DONE';
+
+function masque(_uid,_nick,_img,_conn,_pcnt){
     var m_pcount = _pcnt;
     var m_jcount = 0;
     var m_owner = _uid;
-    var m_data = init(_uid,_nick,_img);
+    var m_data = {};
+    var m_conns = {};
     var m_gameid = 'GID'+
     var m_gamestatus = GAME_STATUS_INIT;
 
-    this.join = function(_uid,_nick,_img){
-        
+    this.join = function(_uid,_nick,_img,_conn){
+        if (m_gamestatus==GAME_STATUS_ONGOING){
+            _conn.send(JSON.stringify({name:GAME_MSG_FULL}));
+        }
+        else if(m_gamestatus==GAME_STATUS_DONE){
+            _conn.send(JSON.stringify({name:GAME_MSG_DONE}));
+        }
+        else{
+            sitdown(_uid,_nick,_img,_conn);
+            if (m_jcount==m_pcount){
+                m_gamestatus = GAME_STATUS_ONGOING;
+                send_all(GAME_MSG_START);
+            }
+            else{
+                send_all(GAME_MSG_JOIN);
+            }
+        }
+    }
+    
+    function exit(_seatno){
+        if (m_data.gamestatus==GAME_STATUS_INIT){
+            m_data.seats[_seatno].userid = null;
+            m_data.seats[_seatno].nick = null;
+            m_data.seats[_seatno].img = null;
+            m_data.seats[_seatno].conn = null;
+            m_data.seats[_seatno].good = GAME_PLAYER_STATUS_EXIT;
+            m_jcount--;
+            if (m_jcount==0){
+                m_data.gamestatus = GAME_STATUS_DONE;
+            }
+            else{
+                send_all(GAME_MSG_JOIN);
+            }
+        }
+        else if (m_data.gamestatus==GAME_STATUS_ONGOING){
+            m_data.seats[_seatno].good = GAME_PLAYER_STATUS_EXIT;
+            m_data.seats[_seatno].conn.send(JSON.stringify({name:GAME_MSG_EXIT}));
+        }
+        else{
+            m_data.seats[_seatno].conn.send(JSON.stringify({name:GAME_MSG_EXIT}));
+        }
     }
 
-    function init(_uid,_nick,_img){
+    function init(_uid,_nick,_img,_conn){
         var cards = undefined;
         if (m_pcount<=4){
             m_pcount=4;
@@ -63,14 +106,13 @@ function masque(_uid,_nick,_img,_pcnt){
             cards[pos][0] = c;
         }
 
-        var game = new Object;
-        game.fine = 0;
+        m_data['fine'] = 0;
         if (m_pcount==4){
-            game.T1 = cards[4][1];
-            game.T2 = cards[5][1];
+            m_data['T1'] = cards[4][1];
+            m_data['T2'] = cards[5][1];
         }
         else if (m_pcount==5){
-            game.T1 = cards[5][1];
+            m_data['T1'] = cards[5][1];
         }
         var seats = new Object;
         for (var j=0;j<cards.length;j++){
@@ -83,15 +125,15 @@ function masque(_uid,_nick,_img,_pcnt){
                 s.good = GAME_PLAYER_STATUS_WAITING;
                 s.money = GAME_PLAYER_INIT_MONEY;
                 seats[cards[j][1]] = s;
-                seats[cards[j][1]] = s;
             }
         }
-        game.seats = seats;
+        m_data['seats'] = seats;
         var sno = 'S'+Math.ceil(Math.random()*m_pcount);
-        game.seats[sno].userid = _uid;
-        game.seats[sno].nick = _nick;
-        game.seats[sno].img = _img;
-        return game;
+        m_data.seats[sno].userid = _uid;
+        m_data.seats[sno].nick = _nick;
+        m_data.seats[sno].img = _img;
+        m_conns[sno] = _conn;
+        m_jcount = 1;
     }
     
     function next(_seat){
@@ -110,6 +152,62 @@ function masque(_uid,_nick,_img,_pcnt){
             prevno = m_pcount;
         }
         return 'S'+prevno;
+    }
+    
+    function create_msg(_name){
+        var msg = new Object;
+        msg.name = _name;
+        msg.gamestatus = m_gamestatus;
+        msg.gameid = m_gameid;
+        msg.seats = m_data.seats;
+        return msg;
+    }
+    
+    function send_all(_name){
+        for (var s in m_data.seats){
+            if (m_data.seats[s].conn!=null){
+                m_data.seats[s].conn.send(JSON.stringify(create_msg(_name)));
+            }
+        }
+    }
+
+    function send_start(){
+        var _players = [];
+        for (var s in m_data.seats){
+            if (m_data.seats[s].userid!=null){
+                _players.push({seatno:s,money:m_data.seats[s].money,card:m_data.seats[s].card,fine:m_data.fine});
+            }
+        }
+        for (var s in m_data.seats){
+            if (m_data.seats[s].conn!=null){
+                var msg = create_msg(GAME_MSG_START);
+                msg.players =_players;
+                m_data.seats[s].conn.send(JSON.stringify(msg));
+            }
+        }
+    }
+    
+    function send_full(_conn){
+        _conn.send(JSON.stringify({name:GAME_MSG_FULL}));
+    }
+
+    function sitdown(_uid,_nick,_img,_conn){
+        var seq = 0;
+        var inx = Math.ceil(Math.random()*(m_pcount-m_jcount));
+        for (var s in m_data.seats){
+            var i = Math.ceil(Math.random()*(m_pcount-m_jcount));
+            if (m_data.seats[s].userid!=null){
+                seq++;
+                if (seq==inx){
+                    m_data.seats[s].userid = _uid;
+                    m_data.seats[s].nick = _nick;
+                    m_data.seats[s].img = _img;
+                    m_conns[s] = _conn;
+                    m_jcount++;
+                    break;
+                }
+            }
+        }
     }
 }
 
